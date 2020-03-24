e0e3180fdb35f1c84a4f42f16722eeec5b3b8bc6
diegomtzb <diegomtzb@hotmail.com>
2020-03-24 10:33:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# material-all-examples-p5krug
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/material-all-examples-p5krug)
---
+++ guides/gettingstarted.guide.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import * as hljs from 'highlight.js';
+
+@Component({
+  selector: 'gettingstarted-guide',
+  templateUrl: 'gettingstarted.guide.html'
+})
+export class GettingStartedGuide {
+}
